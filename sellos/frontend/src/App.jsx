d78e251be4613ba64feba0afca51b6766a014724
Gonzalo Martino <gmartino10.cabj@gmail.com>
2025-10-22 18:47:13 -0300

    Otro import

--- sellos/frontend/src/App.jsx
+++ sellos/frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
 // Contexto y Hooks
 import { CartProvider, useCart } from "./contexts/CartContext";
